507133ce0e36226d2ecb84ea57ba0f3773320a91
melt44 <guilherme44pimenta@gmail.com>
2026-05-25 20:01:14 -0300

    Primeiros passos

---
+++ src/main/java/br/com/itau/desafio/model/transacao.java
@@ -0,0 +1,18 @@
+package br.com.itau.desafio.model;
+import java.time.OffsetDateTime;
+
+public class transacao {
+    private float valor;
+    private OffsetDateTime dataHora;
+
+
+    public float getValor() {
+        return valor;
+    }
+
+    public OffsetDateTime getDataHora() {
+        return dataHora;
+    }
+
+
+}
