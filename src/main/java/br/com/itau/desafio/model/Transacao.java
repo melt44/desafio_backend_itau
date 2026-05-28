4de2c0872ae95406bec9da448e267cc2556dd0f4
melt44 <guilherme44pimenta@gmail.com>
2026-05-28 12:56:44 -0300

    Conclusão do pacote model

---
+++ src/main/java/br/com/itau/desafio/model/Transacao.java
@@ -0,0 +1,32 @@
+package br.com.itau.desafio.model;
+import java.time.OffsetDateTime;
+
+public class Transacao {
+    private double valor;
+    private OffsetDateTime dataHora;
+
+    public Transacao() {
+    }
+
+    public Transacao(double valor, OffsetDateTime dataHora) {
+        this.valor = valor;
+        this.dataHora = dataHora;
+    }
+
+    public void setValor(double valor) {
+        this.valor = valor;
+    }
+
+    public void setDataHora(OffsetDateTime dataHora) {
+        this.dataHora = dataHora;
+    }
+
+    public double getValor() {
+        return valor;
+    }
+
+    public OffsetDateTime getDataHora() {
+        return dataHora;
+    }
+    
+}
